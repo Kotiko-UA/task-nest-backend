c507cfacb0d87e94805c92aa1fe1832a0826e5dc
kotiko <megasuslik666@gmail.com>
2024-02-28 17:45:09 +0200

    add env

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,29 @@ import { AppService } from './app.service';
 import { TaskModule } from 'src/task/task.module';
 import { UserModule } from 'src/user/user.module';
 import { AuthController } from 'src/auth/auth.controller';
-import { ConfigModule } from '@nestjs/config';
-import configuration from '../configuration';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ isGlobal: true, load: [configuration] }),
     TaskModule,
-    UserModule,
     AuthController,
+    UserModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configServise: ConfigService) => ({
+        type: 'postgres',
+        host: configServise.get('DB_HOST'),
+        port: configServise.get('DB_PORT'),
+        username: configServise.get('DB_USERNAME'),
+        password: configServise.get('DB_PASSWORD'),
+        database: configServise.get('DB_NAME'),
+        synchronize: true,
+        entities: [__dirname + '/**/*.entity${.js,.ts}'],
+      }),
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
