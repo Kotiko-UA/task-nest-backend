3c940ce42506f312fd90004703bd6beee9623194
kotiko <megasuslik666@gmail.com>
2024-02-28 15:28:22 +0200

    add env config

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,11 +1,11 @@
 import { Controller, Get } from '@nestjs/common';
 import { UserService } from './user.service';
 
-@Controller('user')
+@Controller('users')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  @Get()
+  @Get('all')
   getUsers() {
     return this.userService.getUsers();
   }
