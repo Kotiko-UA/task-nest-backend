094e267d8214e43579ac4e9d3674d093eb399862
kotiko <megasuslik666@gmail.com>
2024-02-29 16:10:59 +0200

    add auth strategy

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,16 +1,18 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UserService } from 'src/user/user.service';
 import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
   constructor(private readonly userService: UserService) {}
-  async validateUser(email: string, pass: string): Promise<any> {
+  async validateUser(email: string, password: string): Promise<any> {
     const user = await this.userService.findOne(email);
-    const isMatch = await bcrypt.compare(pass, user.password);
-    if (user && isMatch) {
-      const { password, ...result } = user;
-      return result;
+    const passwordCompare = await bcrypt.compare(password, user.password);
+    if (!passwordCompare) {
+      throw new UnauthorizedException('email or password wrong');
+    }
+    if (user && passwordCompare) {
+      return user;
     }
     return null;
   }
